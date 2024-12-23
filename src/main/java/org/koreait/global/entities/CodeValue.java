57384baa2359e1955f1126b679eb7f71687a5bf6
PaB1010 <mudodd@naver.com>
2024-12-23 17:21:08 +0900

    CodeValue

---
+++ src/main/java/org/koreait/global/entities/CodeValue.java
@@ -0,0 +1,29 @@
+package org.koreait.global.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 설정 항목 기본 값
+ * Key & Value 쌍 Map 형태로 저장
+ *
+ */
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class CodeValue {
+
+    @Id
+    @Column(name = "_CODE", length = 45)
+    private String code;
+
+    @Lob
+    @Column(name = "_VALUE")
+    private String value;
+}
