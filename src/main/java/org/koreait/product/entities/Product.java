41ee1730a29d1f613af0d2573bde79f38ff9c678
PaB1010 <mudodd@naver.com>
2025-01-06 17:59:56 +0900

    Product Entity

--- src/main/java/org/koreait/product/entities/Product.java
+++ src/main/java/org/koreait/product/entities/Product.java
@@ -1,54 +1,51 @@
-package org.koreait.admin.product.controllers;
+package org.koreait.product.entities;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.koreait.file.entities.FileInfo;
+import org.koreait.global.entities.BaseMemberEntity;
 import org.koreait.product.constants.DiscountType;
 
 import java.util.List;
 
-/**
- * 상품 커맨드 객체
- */
 @Data
-public class RequestProduct {
-    
-    private String mode;
+@Entity
+public class Product extends BaseMemberEntity { // 주로 관리자가 이용할 것이라 BaseMemberEntity 상속
 
-    // 상품 번호 - 수정시 필수 필요
+    // 상용 솔루션의 경우 seq 가 아닌 상품 별도 관리 코드가 있음
+
+    @Id
+    @GeneratedValue
     private Long seq;
 
     // true : 소비자 페이지에도 상품 노출
     // false : 관리자 페이지에만 상품 노출
-    // @NotNull
+    // @Column(nullable = false)
     private boolean open;
 
-    @NotBlank
+    @Column(length = 45, nullable = false)
     private String gid;
 
     // 상품명
-    @NotBlank
+    // byte 150, 한글은 3byte 이므로 한글 50자
+    @Column(length = 150, nullable = false)
     private String name;
 
     // 상품 요약 설명
     private String summary;
 
-    // 상품 상세 설명 - Editor 연동 작업 예정
+    // 상품 상세 설명
+    @Lob
     private String description;
 
-    // 소비자가 - 취소선 그려서 표기
-    private int consumerPrice;
-
-    // 판매가
-    // @NotBlank 아닌 이유 = 배송비만 받고 파는 사은품이라던가 하는 경우에는 판매가가 없을 수 있음
-    private int salePrice;
-    
     // 할인 종류
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
     private DiscountType discountType;
 
     // 정가 할인 금액(1000) || 할인율(10.5%)
     private double discount;
-    
+
     // 최대 할인 금액
     private int maxDiscount;
 
@@ -59,14 +56,17 @@ public class RequestProduct {
     private int maxPoint;
 
     // 상품 상세 메인 이미지
-    // gid = main
+    // 2차 가공용, gid = main
+    @Transient
     private List<FileInfo> mainImages;
 
     // 상품 목록 이미지
-    // gid = list
+    // 2차 가공용, gid = list
+    @Transient
     private List<FileInfo> listImages;
 
     // 상세 설명 이미지
-    // gid = editor
+    // 2차 가공용, gid = editor
+    @Transient
     private List<FileInfo> editorImages;
 }
